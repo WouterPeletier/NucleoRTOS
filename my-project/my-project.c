1a3c931bfd4d1e38da4a527827de3fa860be00f8
Wouter Peletier <wouter@avy.eu>
2023-02-17 14:37:53 +0100

    libopencm3, with blink sketch for stm32f446RET6

--- my-project/my-project.c
+++ my-project/my-project.c
@@ -1,9 +1,27 @@
-#include "api.h"
-#include "api-asm.h"
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/gpio.h>
 
-int main(void) {
-	/* add your own code */
-	uint32_t rev = 0xaabbccdd;
-	rev = rev_bytes(rev);
-	return my_func(rev);
+static void gpio_setup(void)
+{
+	rcc_periph_clock_enable(RCC_GPIOA);
+	
+	gpio_mode_setup(GPIOA, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO5);
+}
+
+int main(void)
+{
+	int i;
+
+	gpio_setup();
+
+	/* Blink the LED (PA5) on the board. */
+	while (1) {
+		
+		gpio_toggle(GPIOA, GPIO5);	/* LED on/off */
+		for (i = 0; i < 1000000; i++) {	/* Wait a bit. */
+			__asm__("nop");
+		}
+	}
+
+	return 0;
 }
